8f394a17b2983006ba27077af0421c3b6f0d3a78
ghkdwls30 <koreano39@naver.com>
2017-10-19 21:25:57 +0900

    dfasdf

--- client/app/controllers/board.controller.js
+++ client/app/controllers/board.controller.js
@@ -38,34 +38,12 @@ app.controller('boardController',  [ '$scope', '$http', '$location', 'boardServi
 
 app.controller('boardViewController',  [ '$scope', '$routeParams', 'boardService', function( $scope, $routeParams, boardService) {
     
-   
 
-    $scope.post = 
-        {
-        "id": 1234567890,
-        "name": "2017년 회의록",
-        "creator": {
-          "id": 1234567890,
-          "name": "홍길동"
-        },
-        "createdat": "2017/10/01",
-        "lastmodifiedat": "2017/10/11",
-        "contents": "Winter 프로젝트 시작합니다.",
-        "viewcount": 3,
-        "attatch": [
-          {
-            "id": 1234567890,
-            "name": "file1",
-            "link": "http://127.0.0.1/blob/blob0/file1"
-          }
-        ]
-      };
-    
-    // var callback = function( data){
-    //     $scope.post = data;     
-    // } 
+     var callback = function( data){
+         $scope.post = data;     
+     } 
 
-    //boardService.find( id, callback);    
+    boardService.find( id, callback);    
 
 }]);
     
