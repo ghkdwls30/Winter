15cd8e8485b659c4e9514466222465861584441d
daymisono <flower_op4@naver.com>
2017-10-25 20:01:54 +0900

    123

--- server/server.js
+++ server/server.js
@@ -1,53 +1,26 @@
 var express = require('express');
 var app = express();
-//var router = express.Router();
-//var BoardContents = require('./models/boardsSchema');
-var mongoose = require('mongoose'); 
 
 var mysql = require('mysql');
-global.yhbatis = require('yhbatis').yhbatis;
- 
 global.pool = mysql.createPool({
-    host    :'localhost',
-    port : 3306,
-    user : 'ghostone',
-    password : 'ghostone',
-    connectionLimit:20,
-    waitForConnections:true,
+    host     : 'localhost',
+    user     : 'root',
+    password : 'rhtmxm',
+    database : 'database',
     typeCast : true,
     multipleStatements: true
 });
- 
-yhbatis.createYHBatis(
-[
-    "d:/node/workspace/Winter/server/sql.xml"
-]);
- 
 
-//app.use(express.static('client'));
-//app.use('/node_modules', express.static('node_modules'));
+var mybatis = require('mybatisnodejs');
 
-app.get('/', function (req, res) {
-  var mapping = {};
-  mapping.account = 'admin';
-  var result = yhbatis.sqlSection("selectUser",mapping,function(row, callback){
-     console.log(row);
-  });
-  res.send('Hello World!');
-});
+app.use(mybatis.Contexto.domainMiddleware);
+app.use(mybatis.Contexto.middlewareOnError);
 
-// npm install mongoose --save
-// npm install body-parser --save
-//mongoose.connect('mongodb://localhost/boards');
-//var db =mongoose.connection;
-//db.on('error', console.error.bind(console, 'connection error:'));
-//db.once('open', function() {
-//  console.log("connected");
-//});
+var dir_xml = 'c:/node/workspace/WINTER/server/sql/'
 
-//var boards = require('./routes/contents');
-//app.use('/boards', boards);
+var sessionFactory  = new mybatis.Principal().processe(dir_xml);
+global.sessionFactory = sessionFactory;
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+sessionFactory.selecioneUm('query_question.selectuser', {id: 1}, pool, function(user) {
+    //console.log(user);
+ });
