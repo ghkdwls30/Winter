6501de580abfbcb3b08ef48e8a7c58f0c3e37957
daymisono <flower_op4@naver.com>
2017-10-28 15:43:34 +0900

    123

--- server/server.js
+++ server/server.js
@@ -1,32 +1,17 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+var BoardContents = require('./models/boardsSchema');
 
-var mysql = require('mysql');
-global.pool = mysql.createPool({
-    host    :'localhost',
-    port : 3306,
-    user : 'ghostone',
-    password : 'ghostone',
-    connectionLimit:20,
-    waitForConnections:true,
-    typeCast : true,
-    multipleStatements: true
-});
- 
-var mybatis = require('mybatisnodejs');
+app.use(express.static('client'));
+app.use('/node_modules', express.static('node_modules'));
 
-app.use(mybatis.Contexto.domainMiddleware);
-app.use(mybatis.Contexto.middlewareOnError);
-
-var dir_xml = 'c:/node/workspace/WINTER/server/sql/';
-
-var sessionFactory  = new mybatis.Principal().processe(dir_xml);
-global.sessionFactory = sessionFactory;
-
-sessionFactory.selecioneUm('query_question.selectUser', {id: 1}, pool, function(user) {
-  //console.log(user);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
+var boards = require('./routes/contents');
+app.use('/boards', boards);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
